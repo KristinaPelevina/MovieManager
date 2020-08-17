eb1d1a3540efecc18448846d7587c2346b4ac8fc
Kristina Pelevina <christya_13.07@mail.ru>
2020-08-17 12:40:54 +0600

    Added MovieRepository.java and MovieRepositoryTest.java

---
+++ src/main/java/ru/netology/repository/MovieRepository.java
@@ -0,0 +1,47 @@
+
+package ru.netology.repository;
+
+import ru.netology.domain.MovieItem;
+
+public class MovieRepository {
+    private MovieItem[] movies = new MovieItem[0];
+
+    public MovieItem[] findAll() {
+        return movies;
+    }
+
+    public void save(MovieItem movie) {
+        int length = movies.length + 1;
+        MovieItem[] tmp = new MovieItem[length];
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = movie;
+        movies = tmp;
+    }
+
+    public MovieItem findById(int id) {
+        for (MovieItem movie : movies) {
+            if (movie.getId() == id) {
+                return movie;
+            }
+        }
+        return null;
+    }
+
+    public void removeById(int id) {
+        int length = movies.length - 1;
+        MovieItem[] tmp = new MovieItem[length];
+        int index = 0;
+        for (MovieItem movie : movies) {
+            if (movie.getId() != id) {
+                tmp[index] = movie;
+                index++;
+            }
+        }
+        movies = tmp;
+    }
+
+    public void removeAll() {
+        movies = new MovieItem[0];
+    }
+}
---
+++ src/test/java/ru/netology/repository/MovieRepositoryTest.java
@@ -0,0 +1,127 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.MovieItem;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieRepositoryTest {
+    MovieRepository repository = new MovieRepository();
+    private MovieItem first = new MovieItem(1, "Бладшот", "боевик");
+    private MovieItem second = new MovieItem(2, "Вперёд", "мультфильм");
+    private MovieItem third = new MovieItem(3, "Отель «Белград»", "комедия");
+    private MovieItem fourth = new MovieItem(4, "Джентльмены", "боевик");
+    private MovieItem fifth = new MovieItem(5, "Человек-невидимка", "ужасы");
+    private MovieItem sixth = new MovieItem(6, "Тролли. Мировой тур", "мультфильм");
+    private MovieItem seventh = new MovieItem(7, "Номер один", "комедия");
+    private MovieItem eighth = new MovieItem(8, "Тихое место 2", "фантастика");
+    private MovieItem ninth = new MovieItem(9, "Мулан","ужасы");
+    private MovieItem tenth = new MovieItem(10,"Форсаж 10","драма");
+
+    @Test
+    public void shouldFindAllFilms() {
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+        repository.save(fourth);
+        repository.save(fifth);
+        repository.save(sixth);
+        repository.save(seventh);
+        repository.save(eighth);
+        repository.save(ninth);
+        repository.save(tenth);
+        MovieItem[] expected = new MovieItem[]{first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth};
+        MovieItem[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnEmptyIfNoFilms() {
+        MovieItem[] expected = new MovieItem[]{};
+        MovieItem[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSaveFilms() {
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+        repository.save(fourth);
+        repository.save(fifth);
+        MovieItem[] expected = new MovieItem[]{first, second, third, fourth, fifth};
+        MovieItem[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindIfFilmExists() {
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+        repository.save(fourth);
+        repository.save(fifth);
+        int requestedId = 4;
+        repository.findById(requestedId);
+        MovieItem expected = fourth;
+        MovieItem actual = repository.findById(requestedId);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnNullIfFilmNotExists() {
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+        repository.save(fourth);
+        repository.save(fifth);
+        int requestedId = 6;
+        repository.findById(requestedId);
+        MovieItem expected = null;
+        MovieItem actual = repository.findById(requestedId);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveIfExists() {
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+        repository.save(fourth);
+        repository.save(fifth);
+        int removeId = 5;
+        repository.removeById(removeId);
+        MovieItem[] actual = repository.findAll();
+        MovieItem[] expected = new MovieItem[]{first, second,third, fourth};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Disabled
+    @Test
+    public void shouldNotRemoveIfNotExists() {
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+        repository.save(fourth);
+        repository.save(fifth);
+        int removeId = 6;
+        repository.removeById(removeId);
+        MovieItem[] expected = new MovieItem[]{first, second,third, fourth, fifth};
+        MovieItem[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveAll() {
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+        repository.save(fourth);
+        repository.save(fifth);
+        repository.removeAll();
+        MovieItem[] expected = new MovieItem[]{};
+        MovieItem[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+}
